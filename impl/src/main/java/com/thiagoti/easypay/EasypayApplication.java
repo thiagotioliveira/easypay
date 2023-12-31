25a184f419c8b0aa2de16890eecc05d22875f814
THIAGO OLIVEIRA <t.oliveira@kigroup.de>
2023-12-31 17:31:50 +0000

    initial commit

---
+++ impl/src/main/java/com/thiagoti/easypay/EasypayApplication.java
@@ -0,0 +1,12 @@
+package com.thiagoti.easypay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasypayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasypayApplication.class, args);
+    }
+}
---
+++ impl/src/test/java/com/thiagoti/easypay/EasypayApplicationTests.java
@@ -0,0 +1,11 @@
+package com.thiagoti.easypay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasypayApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
